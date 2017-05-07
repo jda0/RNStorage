dc3f62131bd6ddb29529aac0ad4a2b8caa57949d
J Da <da_j@live.co.uk>
2017-05-08 00:57:35 +0100

    Update and rename todoItems.js to shoppingItems.js

--- js/reducers/shoppingItems.js
+++ js/reducers/shoppingItems.js
@@ -2,8 +2,8 @@ const DEFAULT_STATE = {}
 
 export default (state = DEFAULT_STATE, {type, payload} = {}) => {
   switch (type) {
-    case 'TODO_ITEM_ADDED':
-    case 'TODO_ITEM_DELETED':
+    case 'SHOPPING_ITEM_ADDED':
+    case 'SHOPPING_ITEM_DELETED':
       return {...state, lastModified: Date.now()}
     default:
       return state
